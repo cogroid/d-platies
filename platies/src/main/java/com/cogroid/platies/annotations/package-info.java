095223329d64f9de11785cc1df0b55f66da4375a
cogroid <code@cogroid.com>
2022-05-26 21:47:07 -0400

    Platies 2022-05-26 21:47

---
+++ platies/src/main/java/com/cogroid/platies/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link com.cogroid.platies.Platies}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.cogroid.platies.annotations;
---
+++ platies/src/main/java/com/cogroid/platies/stream/TagJsonReader.java
@@ -0,0 +1,37 @@
+/* CD
+ *
+ * Copyright (C) 2022 Dinh Thoai Tran <progrocus@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * CD
+ */
+
+package com.cogroid.platies.stream;
+
+import java.io.Reader;
+import java.io.StringReader;
+
+public abstract class TagJsonReader extends JsonReader {
+    public TagJsonReader(String json) {
+        super(new StringReader(json));
+    }
+
+    public TagJsonReader(Reader in) {
+        super(in);
+    }
+
+    public abstract Object tagValue(String value);
+
+    public abstract String tagField(String field);
+}
